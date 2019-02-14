9f8b8da857adb2d4eb1cbe7ed0e40f4692aa7204
William A. Kennington III <william@wkennington.com>
2019-02-14 15:20:32 -0800

    wip

--- pkgs/all-pkgs/c/ceph/default.nix
+++ pkgs/all-pkgs/c/ceph/default.nix
@@ -1,323 +1,234 @@
 { stdenv
-, autoconf
-, automake
-, ensureNewerSourcesHook
-, fetchgit
-, fetchTritonPatch
+, cmake
 , fetchurl
-, git
-, libtool
-, perl
-, pythonPackages
-, which
+, protobuf-cpp
+, python3Packages
 , yasm
 
 , boost
-, bzip2
+, c-ares
+, cryptopp
 , curl
 , expat
 , fcgi
 , fuse_2
-, gperftools
+, gnutls
+, gperf
 , jemalloc
 , keyutils
+, krb5_lib
 , leveldb
 , libaio
-, libatomic_ops
-, libedit
-, libs3
-, libxml2
-, linux-headers
+, liboath
 , lz4
-, nss
 , nspr
+, nss
 , openldap
 , openssl
+, parted
+, rdma-core
 , rocksdb
 , snappy
 , systemd_lib
 , util-linux_lib
 , xfsprogs_lib
-, zfs
 , zlib
+, zstd
 
 , channel
 }:
 
 let
   inherit (stdenv.lib)
-    optionals
-    optionalString
-    versionAtLeast
-    versionOlder;
+    concatMapStrings
+    head
+    replaceChars;
 
-  sources = (import ./sources.nix).${channel};
+  sources = (import ./sources.nix)."${channel}";
 
   inherit (sources)
     version;
 
-  hasXio = versionAtLeast version "9.0.3";
-
-  hasRocksdb = versionAtLeast version "9.0.0" && !hasStaticRocksdb;
-
-  hasStaticRocksdb = versionAtLeast version "10.0.1";
-
-  # Malloc implementation (can be jemalloc or tcmalloc)
-  malloc = if versionAtLeast version "10.0.4" || !hasStaticRocksdb then jemalloc else gperftools;
-in
-
-stdenv.mkDerivation rec {
-  name="ceph-${version}";
-
   src = fetchurl {
-    url = "https://github.com/wkennington/ceph/releases/download/${version}/${name}.tar.xz";
+    url = "https://github.com/wkennington/ceph/releases/download/${version}/ceph-${version}.tar.xz";
     inherit (sources) sha256;
   };
 
-  patches = optionals (versionOlder version "9.0.0") [
-    (fetchTritonPatch {
-      rev = "3e20a6c39775b724eff44af93f08b38205be1f5b";
-      file = "ceph/fix-pgrefdebugging.patch";
-      sha256 = "11xn226mlzh6c13j9h1xavr9pnnfvkykkxzmf7c0w7hqm3w8r0gs";
-    })
+  cephDisk = python3Packages.buildPythonPackage {
+    name = "ceph-disk-${version}";
+
+    inherit src;
+
+    pythonPath = [
+      parted
+    ];
+
+    prePatch = ''
+      cd src/ceph-disk
+    '';
+
+    postPatch = ''
+      sed -i 's,/bin/\(u\|\)mount,\1mount,g' ceph_disk/main.py
+      sed -i 's,/sbin/blkid,blkid,g' ceph_disk/main.py
+    '';
+  };
+
+  cephVolume = python3Packages.buildPythonPackage {
+    name = "ceph-volume-${version}";
+
+    inherit src;
+
+    prePatch = ''
+      cd src/ceph-volume
+    '';
+
+    postPatch = ''
+      grep -q 'locations = (' ceph_volume/util/system.py
+      sed -i "/locations = (/a\        '/run/current-system/sw/bin'," ceph_volume/util/system.py
+    '';
+  };
+
+  boosts = [
+    boost
+    python3Packages.boost
   ];
+
+  boost' = stdenv.mkDerivation {
+    name = "ceph-combined-${(head boosts).name}";
+
+    buildCommand = ''
+      mkdir -p "$out"
+      cp -ans --no-preserve=mode ${concatMapStrings (n: "${n.lib}/lib ") boosts}"$out"
+      mkdir -p "$out"/nix-support
+      echo '${concatMapStrings (n: "${n.dev} ") boosts}' >"$out"/nix-support/propagated-native-build-inputs
+      echo 'NIX_LDFLAGS="${concatMapStrings (n: "-rpath ${n.lib}/lib ") boosts} $NIX_LDFLAGS"' \
+        >"$out"/nix-support/setup-hook
+    '';
+  };
+in
+stdenv.mkDerivation rec {
+  name = "ceph-${version}";
+
+  src = ../../../../../ceph.tar.xz;
 
   nativeBuildInputs = [
-    autoconf
-    automake
-    (ensureNewerSourcesHook { year = "1980"; })
-    git
-    libtool
-    perl
-    pythonPackages.python
-    pythonPackages.wrapPython
-    which
+    cmake
+    #protobuf-cpp
+    python3Packages.sphinx
+    python3Packages.cython
+    python3Packages.python
     yasm
-  ] ++ optionals (versionAtLeast version "9.0.2") [
-    pythonPackages.setuptools
-    #pythonPackages.sphinx # Used for docs
-  ] ++ optionals (versionAtLeast version "10.0.2") [
-    pythonPackages.cython
   ];
 
-  pythonPath = [
-    pythonPackages.flask
-    pythonPackages.itsdangerous
-    pythonPackages.jinja2
-    pythonPackages.werkzeug
-  ];
-
-  buildInputs = pythonPath ++ [
-    boost
-    libxml2
-    libatomic_ops
-    malloc
-    zlib
-    bzip2
-    linux-headers
-    nss
-    nspr
-    util-linux_lib
-    systemd_lib
-    keyutils
-    libaio
-    xfsprogs_lib
-    zfs
-    snappy
-    leveldb
-    fcgi
-    expat
+  buildInputs = [
+    boost'
+    #c-ares
+    #cryptopp
     curl
+    expat
     fuse_2
-  ] ++ optionals (versionOlder version "10.0.4") [
-    libedit
-  ] ++ optionals (versionAtLeast version "10.0.0") [
+    #gnutls
+    gperf
+    jemalloc
+    keyutils
+    krb5_lib
+    leveldb
+    libaio
+    liboath
     lz4
-  ] ++ optionals hasRocksdb [
-    rocksdb
-  ] ++ optionals (versionOlder version "9.1.0") [
-    libs3
-  ] ++ optionals (versionAtLeast version "10.1.0") [
+    nspr
+    nss
     openldap
     openssl
+    #protobuf-cpp
+    rdma-core
+    rocksdb
+    snappy
+    systemd_lib
+    util-linux_lib
+    xfsprogs_lib
+    zlib
   ];
 
-  # We don't want any of the debug info built as it is huge
-  # and slow to generate.
-  CFLAGS="-O2";
-  CXXFLAGS="-O2";
+  # Needed by the ceph command line
+  pythonPath = [
+  ];
 
   postPatch = ''
-    export PYTHONPATH="$(toPythonPath $lib):$(toPythonPath $out):$PYTHONPATH"
+    # We manually set the version of ceph directly so we don't have to depend on git
+    sed \
+      -e 's,GIT-NOTFOUND,${version},g' \
+      -e 's,GITDIR-NOTFOUND,${replaceChars ["-" "."] ["" ""] version},g' \
+      -i cmake/modules/GetGitRevisionDescription.cmake
 
-    # Fix zfs pkgconfig detection
-    sed -i 's,\[zfs\],\[libzfs\],g' configure.ac
-
-    # Fix GNU_SOURCE
-    sed -i '/AC_INIT/aAC_GNU_SOURCE' configure.ac
-
-    # Remove anything that depends on virtualenv
-    sed -i '/include ceph-\(disk\|detect-init\)/d' src/Makefile.am
-
-    # Fix sbindir moving
-    sed -i 's,su_sbindir = /sbin,su_sbindir = $(bindir),g' src/Makefile-env.am
-  '' + optionalString (versionAtLeast version "9.1.0") ''
-    # Don't embed debug info as it is huge
-    sed -i 's, -g , ,g' src/Makefile-env.am
-
-    # Fix pythonpath
-    sed -i 's,export PYTHONPATH=,export PYTHONPATH+=:,g' src/Makefile-env.am
-  '' + optionalString (versionAtLeast version "10.1.0" && versionOlder version "10.2.7") ''
-    # Fix LDAP linking
-    ! grep '\(-lldap\|LDAP_LIB\)' src/rgw/Makefile.am
-    sed -i 's,LIBRGW_DEPS +=,\0 -lldap,g' src/rgw/Makefile.am
-  '' + optionalString (versionOlder version "12.0.0") ''
-    patchShebangs src/gmock/gtest/scripts/fuse_gtest_files.py
+    # Use up to date zstd sources
+    unpackFile '${zstd.src}'
+    mv zstd-* src/zstd
   '';
 
   preConfigure = ''
-    # Ceph expects the arch command to be usable during configure
-    # for detecting the assembly type
-    mkdir -p mybin
-    echo "#${stdenv.shell} -e" >> mybin/arch
-    echo "uname -m" >> mybin/arch
-    chmod +x mybin/arch
-    PATH="$PATH:$(pwd)/mybin"
-
-    ./autogen.sh
-
-    # Fix the python site-packages install directory
-    sed -i "s,\$PYTHON\(_EXEC\)\?_PREFIX,$lib,g" configure
-  '';
-
-  configureFlags = [
-    "--disable-silent-rules"
-    "--exec_prefix=\${out}"
-    "--sysconfdir=/etc"
-    "--localstatedir=/var"
-    "--libdir=\${lib}/lib"
-    "--includedir=\${lib}/include"
-    "--with-rbd"
-    "--with-cephfs"
-    "--with-radosgw"
-    "--with-radosstriper"
-    "--with-mon"
-    "--with-osd"
-    "--with-mds"
-    "--enable-client"
-    "--enable-server"
-    "--without-cryptopp"
-    "--with-nss"
-    "--disable-root-make-check"
-    "--without-profiler"
-    "--without-debug"
-    "--disable-coverage"
-    "--with-fuse"
-    "--${if malloc == jemalloc then "with" else "without"}-jemalloc"
-    "--${if malloc == gperftools then "with" else "without"}-tcmalloc"
-    "--disable-pgrefdebugging"
-    "--disable-cephfs-java"
-    "--disable-xio"
-    "--with-libatomic-ops"
-    "--with-ocf"
-    "--without-kinetic"
-    "--${if hasRocksdb then "with" else "without"}-librocksdb"
-    "--${if hasStaticRocksdb then "with" else "without"}-librocksdb-static"
-    "--with-libaio"
-    "--with-libxfs"
-    "--with-libzfs"
-  ] ++ optionals (versionAtLeast version "0.94.3") [
-    "--without-tcmalloc-minimal"
-  ] ++ optionals (versionAtLeast version "9.0.1") [
-    "--without-valgrind"
-  ] ++ optionals (versionAtLeast version "9.0.2") [
-    "--without-man-pages"  # TODO(wkennington): Fix
-  ] ++ optionals (versionAtLeast version "9.0.2" && versionOlder version "10.0.4") [
-    "--with-systemd-libexec-dir=\${out}/libexec"
-  ] ++ optionals (versionOlder version "9.1.0") [
-    "--with-system-libs3"
-    "--with-rest-bench"
-  ] ++ optionals (versionAtLeast version "9.1.0") [
-    "--with-systemd-unit-dir=\${out}/etc/systemd/system"
-    "--without-selinux"  # TODO: Implement
-  ] ++ optionals (versionAtLeast version "9.1.0" && versionOlder version "10.0.4") [
-    "--with-rgw-user=rgw"
-    "--with-rgw-group=rgw"
-  ] ++ optionals (versionAtLeast version "10.0.2") [
-    "--with-cython"
-  ] ++ optionals (versionAtLeast version "10.0.4") [
-    "--with-eventfd"
-    "--without-spdk" # TODO: Implement
-  ] ++ optionals (versionAtLeast version "10.1.0") [
-    "--enable-subman"
-    "--with-openldap"
-  ];
-
-  preBuild = optionalString (versionAtLeast version "9.0.0" && versionOlder version "12.0.0") ''
-    (cd src/gmock; make -j $NIX_BUILD_CORES)
-  '';
-
-  preInstall = ''
-    installFlagsArray+=(
-      "sysconfdir=$out/etc"
-      "submandir=$out/etc/cron.hourly"
-      "localstatedir=$TMPDIR"
+    cmakeFlagsArray+=(
+      "-DCMAKE_INSTALL_INCLUDEDIR=$lib/include"
+      "-DCMAKE_INSTALL_LIBDIR=$lib/lib"
     )
   '';
 
+  cmakeFlags = [
+    "-DWITH_GSSAPI=ON"
+    "-DWITH_SPDK=OFF"
+    "-DWITH_BLUEFS=ON"
+    "-DWITH_LTTNG=OFF"
+    "-DWITH_BABELTRACE=OFF"
+    "-DWITH_RADOSGW_AMQP_ENDPOINT=OFF"
+    "-DDEBUG_GATHER=OFF"
+    "-DWITH_TESTS=OFF"
+    "-DWITH_SYSTEM_ROCKSDB=ON"
+    "-DWITH_SEASTAR=OFF"
+    "-DWITH_SYSTEM_BOOST=ON"
+    "-DWITH_GRAFANA=ON"
+    "-DWITH_MGR_DASHBOARD_FRONTEND=OFF"
+
+    "-DWITH_PYTHON3=ON"
+  ];
+
+  # Ensure we have the correct rpath already to work around
+  # a broken patchelf.
+  preBuild = ''
+    export NIX_LDFLAGS="$NIX_LDFLAGS -rpath $lib/lib -rpath $(pwd)/lib"
+  '';
+
+  postInstall = ''
+    # Move python libraries to lib
+    mv "$out"/lib/python* "$lib"/lib
+    rmdir "$out"/lib
+
+    # Bring in lib as a native build input
+    mkdir -p "$out"/nix-support
+    echo "$lib" > "$out"/nix-support/propagated-native-build-inputs
+  '';
+
+  preFixup = ''
+    wrapPythonPrograms "$out"/bin
+
+    # Contain impure absolute paths
+    find "$lib" -name SOURCES.txt -delete
+  '';
+  
   outputs = [
     "out"
     "lib"
   ];
 
-  postInstall = ''
-    # Bring in lib as a native build input
-    mkdir -p $out/nix-support
-    echo "$lib" > $out/nix-support/propagated-native-build-inputs
-  '' + optionalString (versionAtLeast version "10.1.0") ''
-    # Move all of the python libraries to $lib
-    mv $(toPythonPath $out)/* $(toPythonPath $lib)
-    rm -rf $out/lib
-  '' + optionalString (versionOlder version "10.1.0") ''
-    # Fix the python library loading
-    find $lib/lib -name \*.pyc -or -name \*.pyd -exec rm {} \;
-    for PY in $(find $lib/lib -name \*.py); do
-      LIBS="$(sed -n "s/.*find_library('\([^)]*\)').*/\1/p" "$PY")"
+  disallowedReferences = [
+    boost'
+  ];
 
-      # Delete any calls to find_library
-      sed -i '/find_library/d' "$PY"
-
-      # Fix each find_library call
-      for LIB in $LIBS; do
-        REALLIB="$lib/lib/lib$LIB.so"
-        sed -i "s,\(lib$LIB = CDLL(\).*,\1'$REALLIB'),g" "$PY"
-      done
-
-      # Reapply compilation optimizations
-      NAME=$(basename -s .py "$PY")
-      rm -f "$PY"{c,o}
-      pushd "$(dirname $PY)"
-      python -c "import $NAME"
-      python -O -c "import $NAME"
-      popd
-      test -f "$PY"c
-      test -f "$PY"o
-    done
-  '';
-
-  preFixup = ''
-    wrapPythonPrograms $out/bin
-  '';
-
-  passthru.version = version;
-
-  # FIXME
-  buildDirCheck = false;
+  passthru = {
+    disk = cephDisk;
+    volume = cephVolume;
+  };
 
   meta = with stdenv.lib; {
-    description = "Distributed storage system";
-    homepage = https://ceph.com/;
-    license = licenses.lgpl21;
     maintainers = with maintainers; [
       wkennington
     ];
--- pkgs/all-pkgs/c/ceph/sources.nix
+++ pkgs/all-pkgs/c/ceph/sources.nix
@@ -1,22 +1,22 @@
 {
-  "10" = rec {
-    version = "10.2.11";
-    sha256 = "44638c6e2ea822a40dec3fe6e504972886fe4430f91bc5b4a01f2377f7ba750e";
-  };
   "12" = rec {
-    version = "12.2.11";
-    sha256 = "27bbdaac6ecd593c591fea2d1ec2e5a0c17a1796e249d9cb79105ba7ddc1f6ce";
+    version = "12.2.12";
+    sha256 = "27badaac6ecd593c591fea2d1ec2e5a0c17a1796e249d9cb79105ba7ddc1f6ce";
+  };
+  "14" = rec {
+    version = "14.2.0";
+    sha256 = "27baaaac6ecd593c591fea2d1ec2e5a0c17a1796e249d9cb79105ba7ddc1f6ce";
   };
   "13" = rec {
     version = "13.2.2";
     sha256 = "22cf2ee7aa946ba270dde76028bac5385cd95f62a544a8d482db4dc180219ae8";
   };
   "dev" = rec {
-    version = "12.2.0";
-    sha256 = "ac0778592c32dcf1aeaea73355bbfaa43ec346c0f366cabb5cac936677ca56c1";
+    version = "15.0.0";
+    sha256 = "ac07a8592c32dcf1aeaea73355bbfaa43ec346c0f366cabb5cac936677ca56c1";
   };
   "git" = rec {
-    version = "2017-04-15";
-    sha256 = "e4392e715e3b3f34a9bdb607242b2b16ab0fe01a141c36fa0296faf557011842";
+    version = "2019-04-18";
+    sha256 = "e4392e715eab3f34a9bdb607242b2b16ab0fe01a141c36fa0296faf557011842";
   };
 }
