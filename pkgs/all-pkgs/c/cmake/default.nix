8effdae62c9d83f0e3f3fa6876acce3b6a854df5
William A. Kennington III <william@wkennington.com>
2019-05-27 23:16:15 -0700

    Revert "cmake: Decrease bootstrap"
    This reverts commit ac1c77dead4f926f7eef44cb8186fd3e7f4f6273.

--- pkgs/all-pkgs/c/cmake/default.nix
+++ pkgs/all-pkgs/c/cmake/default.nix
@@ -68,13 +68,6 @@ stdenv.mkDerivation rec {
     sed -i 's,uv-version.h,uv/version.h,' Source/Modules/FindLibUV.cmake
   '' + optionalString (!bootstrap) ''
     sed -i '/CMAKE_USE_SYSTEM_/s,OFF,ON,g' CMakeLists.txt
-  '' + optionalString bootstrap ''
-    sed \
-      -e 's,''${cmake_bootstrap_dir}/cmake,true,' \
-      -e "/\''${CMAKE_BOOTSTRAP_SOURCE_DIR}/iCMAKE_BOOTSTRAP_SOURCE_DIR='$out/share/cmake-${channel}'" \
-      -e "/\''${CMAKE_BOOTSTRAP_BINARY_DIR}/iCMAKE_BOOTSTRAP_BINARY_DIR='$out/bin'" \
-      -i bootstrap
-    cat bootstrap
   '';
 
   preConfigure = optionalString bootstrap ''
@@ -93,17 +86,6 @@ stdenv.mkDerivation rec {
   cmakeFlags = optionals (!bootstrap) [
     "-DCMAKE_USE_SYSTEM_KWIML=OFF"
   ];
-
-  buildPhase = optionalString bootstrap ''
-    true
-  '';
-
-  installPhase = optionalString bootstrap ''
-    mkdir -p "$out"/bin
-    cp Bootstrap.cmk/cmake "$out"/bin
-    mkdir -p "$out"/share/cmake-${channel}
-    cp -r Modules "$out"/share/cmake-${channel}
-  '';
 
   setupHook = ./setup-hook.sh;
   selfApplySetupHook = true;
